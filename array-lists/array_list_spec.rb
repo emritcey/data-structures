d11141ad9585c928e3fe1b3067380695e26b0fde
emritcey <emritcey@gmail.com>
2016-08-22 17:38:54 -0500

    write tests for new

--- array-lists/array_list_spec.rb
+++ array-lists/array_list_spec.rb
@@ -1,5 +1,14 @@
 require_relative 'array_list'
 
 describe ArrayList do
+	it "has a default size" do
+		array = ArrayList.new
+		expect(array.size).to eq 5
+	end 
+	
+	it "can be size based on a users input" do 
+		array = ArrayList.new(10)
+		expect(array.size).to eq 10
+	end 
 
 end
