bc16e40ce047f01d8ea4640dfde08793139cfc63
emritcey <emritcey@gmail.com>
2016-10-25 17:07:52 -0500

    write test for initalized with element

--- linked_list/node_spec.rb
+++ linked_list/node_spec.rb
@@ -1,5 +1,13 @@
 require_relative 'node'
 
 describe Node do
+	describe "new" do 
+
+		it "is initialized with an element" do 
+			node = Node.new(5)
+			expect(node.element).to eq 5
+		end 
+
+	end
 
 end
