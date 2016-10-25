c0f80a4d479d355a92e9a88e59473649c1e0a85a
emritcey <emritcey@gmail.com>
2016-10-25 17:53:22 -0500

    write 2 more tests for initialization to pass

--- linked_list/linked_list_spec.rb
+++ linked_list/linked_list_spec.rb
@@ -7,18 +7,52 @@ describe LinkedList do
 			new_list = LinkedList.new
 			expect(new_list.first_element).to eq nil 
 		end 
+		it "can be initialized with a value" do 
+			new_list = LinkedList.new(5)
+			expect(new_list.first_element).to eq 5
+		end 
+
+		it "can be initialized with a Node" do 
+			new_node = Node.new(5)
+			new_list = LinkedList.new(new_node)
+			expect(new_list.first_element).to eq new_node 
+		end
 	end 
 
 	describe "insert first" do 
+		it "" do 
+		end 
+		it "" do 
+		end
+		it "" do 
+		end
 	end 
 
 	describe "remove first" do 
+		it "" do 
+		end 
+		it "" do 
+		end
+		it "" do 
+		end
 	end 
 
 	describe "insert last" do 
+		it "" do 
+		end 
+		it "" do 
+		end
+		it "" do 
+		end
 	end 
 
 	describe "remove last" do 
+		it "" do 
+		end 
+		it "" do 
+		end
+		it "" do 
+		end
 	end 
 
 end
