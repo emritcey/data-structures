1258858d39c9098c947b667f05461529ff2402e1
emritcey <emritcey@gmail.com>
2016-10-25 17:21:58 -0500

    add all test for 3 Node methods to pass

--- linked_list/node_spec.rb
+++ linked_list/node_spec.rb
@@ -1,13 +1,34 @@
 require_relative 'node'
 
 describe Node do
+	node = Node.new(5)
 	describe "new" do 
 
 		it "is initialized with an element" do 
-			node = Node.new(5)
 			expect(node.element).to eq 5
 		end 
 
+		it "is initialized with next element = nil" do
+			expect(node.next).to eq nil
+		end 
 	end
+
+	describe "insert after method" do
+		second_node = Node.new(15)
+		it "sets next to a value or object" do
+			node.insert_after(second_node)
+
+			expect(node.next).to be_a_kind_of(Node)
+			expect(node.next).to eq second_node
+			expect(node.next.element).to eq 15
+		end 
+	end 
+
+	describe "remove after method" do
+		it "resets next to nil" do
+			node.remove_after
+			expect(node.next).to eq nil 
+		end 
+	end 
 
 end
