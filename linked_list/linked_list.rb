41729a78424c88b18651e5b4c3ef97aa2a9bf794
emritcey <emritcey@gmail.com>
2016-10-25 17:50:36 -0500

    add test for intialize, write code to pass

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -2,4 +2,10 @@ require_relative 'node'
 
 class LinkedList
 
+	attr_reader :first_element 
+	
+	def initialize(first_element=nil)
+		@first_element = first_element
+	end 
+
 end
--- linked_list/linked_list_spec.rb
+++ linked_list/linked_list_spec.rb
@@ -2,4 +2,23 @@ require_relative 'linked_list'
 
 describe LinkedList do
 
+	describe "new" do 
+		it "is initialized with head object == nil" do 
+			new_list = LinkedList.new
+			expect(new_list.first_element).to eq nil 
+		end 
+	end 
+
+	describe "insert first" do 
+	end 
+
+	describe "remove first" do 
+	end 
+
+	describe "insert last" do 
+	end 
+
+	describe "remove last" do 
+	end 
+
 end
