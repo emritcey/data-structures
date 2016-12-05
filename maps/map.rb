446f3cfbda6a1364299ee82786d10f26cafe8ab6
Emma Ritcey <email": null>
2016-12-05 16:38:23 -0600

    finish map

--- maps/map.rb
+++ maps/map.rb
@@ -1,3 +1,27 @@
 class Map
 
+  def initialize
+    @map = {}
+  end
+
+  def set(key, value)
+    @map[key] = value
+  end
+
+  def get(key)
+    @map[key]
+  end
+
+  def has_key?(key)
+    @map.include?(key)
+  end
+
+  def remove(key)
+    @map.delete(key)
+  end
+
+  def iterate(&block)
+    @map.each(&block)
+  end
+
 end
