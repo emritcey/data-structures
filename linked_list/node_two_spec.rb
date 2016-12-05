c4bd8fef425fff2baf1fc293cd940bdaee9e4cf6
Emma Ritcey <email": null>
2016-12-05 13:41:19 -0600

    add more tests

--- linked_list/node_two_spec.rb
+++ linked_list/node_two_spec.rb
@@ -1,5 +1,28 @@
 require_relative 'node'
 
 describe Node do
+    node = Node.new(4)
+  describe "#new" do
+    it "initializes with an element" do
+      expect(node.element).to eq 4
+    end
+
+    it "initializes next as nil" do
+      expect(node.next).to be_nil
+    end
+
+  end
+
+  describe "#insert_after(node)" do
+    it "changes value of next" do
+      expect {node.insert_after(5)}.to change{node.next}.from(nil).to(5)
+    end
+  end
+
+  describe "#remove_after" do
+    it "changes next to nil" do
+      expect {node.remove_after}.to change{node.next}.from(5).to(nil)
+    end
+  end
 
 end
