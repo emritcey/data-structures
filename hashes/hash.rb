fcb9ee71fa4a79ee5426cf6b61c8564d9d03a0f2
Emma Ritcey <email": null>
2016-12-05 19:17:00 -0600

    add

--- hashes/hash.rb
+++ hashes/hash.rb
@@ -1,3 +1,25 @@
 class Hash
 
+  def initialize
+    @dictionary = {}
+  end
+
+  def set(key,value)
+    @dictionary[key] = value
+  end
+
+  def get(key)
+    @dictionary[key]
+  end
+
+  def has_key?(key)
+
+  end
+
+  def remove(key)
+  end
+
+  def iterate(&block)
+  end
+
 end
--- sets/set.rb
+++ sets/set.rb
@@ -1,3 +1,25 @@
 class MySet
+  def intialize
+    @set = []
+  end
+
+  def add(element)
+    raise Error if @set.contains?(element)
+    @set << element
+  end
+
+  def remove(element)
+    @set.delete(element) if @set.contains?(element)
+  end
+
+  def contains?(element)
+    @set.include?(element)
+  end
+
+  def size
+    @set.length
+  end
+
+
 
 end
